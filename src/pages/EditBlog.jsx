f1c4a96f859e2fafd62a2fffd4e847309c0444b1
Sushant <clashofgames229@gmail.com>
2025-05-14 07:06:46 +0545

    editblog

--- src/pages/EditBlog.jsx
+++ src/pages/EditBlog.jsx
@@ -15,26 +15,26 @@ const EditBlogs = () => {
 
       const {user} = useContext(AuthContext);
 
-    const handleImageChange = (event)=>{
-       const file = event.target.files[0];
-         if(file){
-            setImage(file);
-         }
-    }
+    // const handleImageChange = (event)=>{
+    //    const file = event.target.files[0];
+    //      if(file){
+    //         setImage(file);
+    //      }
+    // }
 
     const handleSubmit = async(values)=>{
 
-      console.log(values);
-        console.log(values,image,user);
-        const formData = new FormData();
-           formData.append("title",values?.title)
-           console.log(formData)
-           formData.append("content",values?.content);
-           if(image) formData.append("image",image);
-           formData.append("author",user?._id);
+      // console.log(values);
+      //   console.log(values,image,user);
+        // const formData = new FormData();
+        //    formData.append("title",values?.title)
+        //    console.log(formData)
+        //    formData.append("content",values?.content);
+        //   //  if(image) formData.append("image",image);
+        //    formData.append("author",user?._id);
 
           try {
-               await axios.put(`https://blog-hqx2.onrender.com/blog/${id}`,formData)
+               await axios.put(`https://blog-hqx2.onrender.com/blog/${id}`,values)
                 navigate("/");
           } catch (error) {
             console.log(error)
@@ -59,10 +59,10 @@ const EditBlogs = () => {
                 <label htmlFor="title" className="text-xl mr-4">Content</label>
                 <Field type="text" name="content" placeholder="enter blog content" className="border" />
 
-                <div className="mt-4">
+                {/* <div className="mt-4">
                     <label htmlFor="Image">Upload blog image</label> <br />
                       <input type="file" name="image" onChange={(event)=>handleImageChange(event)} className="border bg-amber-300" />
-                </div>
+                </div> */}
 
                 <button type="submit" className="p-2 bg-blue-500 rounded-lg text-white mt-10 cursor-pointer">Submit blog</button>
             </Form>
